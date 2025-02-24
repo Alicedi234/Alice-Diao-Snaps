f596e71b964a05309e5fa9e2616b8584ec43bdca
Yawen Diao <yawendiao@gmail.com>
2025-02-23 23:49:24 -0500

    Filter section fixed

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,7 +1,29 @@
-import tags from "../../data/tags.json";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import "./Filter.scss";
 
 export default function FilterButtons({ selectedTag, setSelectedTag }) {
+  const [photos, setPhotos] = useState([]);
+  const [uniqueTags, setUniqueTags] = useState(new Set());
+
+  const url = "https://unit-3-project-c5faaab51857.herokuapp.com/photos?api_key=4cf989da-7d80-4ad0-8dd3-82429cbbb621";
+
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const response = await axios.get(url);
+      setPhotos(response.data);
+
+      const tags = new Set();
+      response.data.forEach(photo => {
+        photo.tags.forEach(tag => {
+          tags.add(tag);
+        });
+      });
+      setUniqueTags(tags);
+    };
+    fetchPhotos();
+  }, []);
+
   const ClickHandler = (tag) => {
     if (selectedTag === tag) {
       setSelectedTag(null);
@@ -9,22 +31,20 @@ export default function FilterButtons({ selectedTag, setSelectedTag }) {
       setSelectedTag(tag);
     }
   };
+
   return (
     <>
-      {tags.map((tag, index) => {
-        return (
-          <button
-            className={`filter__button ${
-              selectedTag === tag ? "fitler__button--active" : ""
-            } `}
-            key={index}
-            onClick={() => ClickHandler(tag)}
-          >
-            {tag}
-          </button>
-        );
-      })}
+      {Array.from(uniqueTags).map((tag, index) => (
+        <button
+          className={`filter__button ${
+            selectedTag === tag ? "filter__button--active" : ""
+          }`}
+          key={index}
+          onClick={() => ClickHandler(tag)}
+        >
+          {tag}
+        </button>
+      ))}
     </>
   );
 }
-
