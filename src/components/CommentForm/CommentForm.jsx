07f7c647d1de207a190d0204b773c77fdcadfc52
Yawen Diao <yawendiao@gmail.com>
2025-02-23 23:00:51 -0500

    form error state

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -6,11 +6,24 @@ import "./CommentForm.scss";
 export default function CommentForm({onSubmit}){
   const [name, setName] = useState("");
   const [comment, setComment] = useState("");
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isCommentValid, setIsCommentValid] = useState(true);
 
   const {id} = useParams();
 
   const submitHandler = async(event) =>{
     event.preventDefault();
+    
+    const isNameEmpty = name.trim() === "";
+    const isCommentEmpty = comment.trim() === "";
+
+    setIsNameValid(!isNameEmpty);
+    setIsCommentValid(!isCommentEmpty);
+
+    if (isNameEmpty || isCommentEmpty) {
+      alert("The field can not be empty");
+      return;
+    }
 
     
     const url = `https://unit-3-project-c5faaab51857.herokuapp.com/photos/${id}/comments?api_key=4cf989da-7d80-4ad0-8dd3-82429cbbb621`;
@@ -32,16 +45,12 @@ export default function CommentForm({onSubmit}){
   return(
     <form onSubmit = {submitHandler} className ="comment">
       <label htmlFor="name" className="comment__label">Name</label>
-      <input  className ="comment__input--name"
-       type="text" id="name" value ={name} 
-      required/>
+      <input  className ={`comment__input--name ${!isNameValid ? "invalid" : ""}`}
+       type="text" id="name" value ={name} onChange ={(event) => setName(event.target.value)} />
       <label htmlFor="comment" className="comment__label">Comment</label>
-      <textarea className ="comment__input--comment" name="comment" value ={comment}
-      required></textarea>
+      <textarea className ={`comment__input--comment ${!isCommentValid ? "invalid" : ""}`} name="comment" value ={comment} onChange={(event)=>setComment(event.target.value)} ></textarea>
       <button className ="comment__button" type = "submit">Submit</button>
     </form>
     
   )
 }
-// onChange ={(event) => setName(event.target.value)}
-// onChange={(event)=>setComment(event.target.value)} 
